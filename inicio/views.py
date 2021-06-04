c9ae838e02fc7d34a18c95282aa284caedfdbe51
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-04 11:33:41 -0500

    cambio la funcion para imprimir mas datos y en urls se añadio una nueva

--- inicio/views.py
+++ inicio/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse('<h1>Hola mundo</h1>')
+
+def another(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return HttpResponse('<h1>Otra pagina</h1>')
