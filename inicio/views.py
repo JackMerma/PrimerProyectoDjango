61595d83da8b169f0dd17002593bab3e7e823c1b
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-07 00:07:20 -0500

    se retorna un HttpResponse para another

--- inicio/views.py
+++ inicio/views.py
@@ -5,10 +5,8 @@ from django.http import HttpResponse
 def myHomeView(request, *args, **kwargs):
     return render(request, "home.html", {})
 
-def another(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return render(request, "home.html", {})
+def another(*args, **kwargs):
+    return HttpResponse('<h1>Esta es otra pagina</h1>');
 
 def letras(request, *arg, **kwargs):
     return render(request, "letters/first.html", {})
