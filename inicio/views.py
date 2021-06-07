0bacdef95b6a155e25c151906235958d9931ccfd
Jackson Merma <jmermap@unsa.edu.pe>
2021-06-06 22:11:17 -0500

    en views, se hizo un render al archivo home.html, desde la base.html se llama al nuevo archivo content.html que solo tiene un pre tag

--- inicio/views.py
+++ inicio/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(*args, **kwargs):
-    return HttpResponse('<h1>Hola mundo</h1>')
+def myHomeView(request, *args, **kwargs):
+    return render(request, "home.html", {})
 
 def another(request, *args, **kwargs):
     print(args, kwargs)
